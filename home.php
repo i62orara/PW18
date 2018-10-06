0955290db8f653194e369b701e2266602f0a9270
i62orara <39118511+i62orara@users.noreply.github.com>
2018-10-06 13:16:44 +0200

    home.php
    Pagina de inicio de la web

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>home</title>
+</head>
+<body style="background-color:white;">
+<?php
+echo <<<_END
+	<h1 align="center" style="background-color:#B2DF65;"> Empleados</h1>
+	<center><a href="empleado.html"> <img src="PW18/P2/web/empleados.jpg" > </img> </a></center>
+_END;
+
+echo <<<_END
+Si desea informacion sobre los empleados pinche en la imagen,
+<br>esta le redireccionara a un enlace con los trabajadores.</p>
+_END;
+
+?>
+</body>
+
+</html>
